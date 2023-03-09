1a1106b5554b89297417567b21289bcb991c52f8
Faith Goins <faithtoriangoins@gmail.com>
2023-03-09 17:35:45 -0600

    called methods to main

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,9 +4,13 @@
     {
         static void Main(string[] args)
         {
-           
-           
-        
+           var whole= methods.Add(1, 2);
+            var points =methods.Add(2.03m, 3.53m);
+            var isMoney = methods.Add(1, 2, true);
+            Console.WriteLine(  $"{whole} and \n {points} \n {isMoney}");
+
+
+
         }
     }
 }
